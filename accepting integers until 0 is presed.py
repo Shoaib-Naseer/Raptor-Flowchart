2b51132aac27f729fb46206cc1edf79474eb576e
shoaib <43854873+Shoaib-Naseer@users.noreply.github.com>
2019-10-05 11:24:34 +0500

    Add files via upload
    Some raptors flowcharts designed by me

---
+++ accepting integers until 0 is presed.py
@@ -0,0 +1,7 @@
+i=0
+sum=0
+n=int(input("enter any number"))
+while n!=0:
+    n=int(input("enter any number"))
+    sum=sum+n
+print(sum)
---
+++ sum of 5 numbers.py
@@ -0,0 +1,7 @@
+i=1
+sum=0
+while i<=5:
+    n=int(input("enter any number"))
+    sum=sum+n
+    i=i+1
+print(sum)
